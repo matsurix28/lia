b256ae5d7833835117013b0e3d4b221a8fe66e86
matsurix28@vaio <matsurix28@gmail.com>
2024-04-27 19:30:23 +0900

    variable

--- lia/detect/extract.py
+++ lia/detect/extract.py
@@ -1,9 +1,10 @@
-from .detect_leaf import extract_leaf_by_color, extract_leaf_by_thresh
+from lia.detect import extract_leaf_by_color, extract_leaf_by_thresh
 
+from lia.basic.get import 
 
 class ExtractLeaf:
     def __init__(self) -> None:
-        
+        pass
         
     def by_thresh():
         pass
