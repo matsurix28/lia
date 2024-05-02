40badc35cda18c75b30d9581746554723d940674
matsurix28@vaio <matsurix28@gmail.com>
2024-05-02 10:47:13 +0900

    0503

---
+++ lia/detect/detect_fvfm.py
@@ -0,0 +1,5 @@
+import easyocr
+
+
+def read_fvfm_value():
+    reader = easyocr.Reader(["en"])
---
+++ tests/test_fvfm.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from lia.detect.detect_fvfm import read_fvfm_value
+
+read_fvfm_value()
+read_fvfm_value()
+read_fvfm_value()
+print("aaa")
