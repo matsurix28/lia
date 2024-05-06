2f8e29395fe074acc7da2f58d9c19fcef6baa38f
matsuri@GOLLIRA <matsurix28@gmail.com>
2024-05-06 09:19:46 +0900

    white background

--- lia/basic/get/cnts.py
+++ lia/basic/get/cnts.py
@@ -1,7 +1,7 @@
 import cv2
 
 # from . import MIN_CNTS_RATIO
-from .consts import MIN_CNTS_RATIO
+from .consts import MIN_CNTS_RATIO, WHITE_BG_THRESH
 
 
 def get_cnts(img, min_cnts_ratio=MIN_CNTS_RATIO):
@@ -23,4 +23,12 @@ def get_cnts(img, min_cnts_ratio=MIN_CNTS_RATIO):
     min_area = (height * width) / min_cnts_ratio
     cnts, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts_list = list(filter(lambda x: cv2.contourArea(x) > min_area, cnts))
+    return cnts_list
+
+
+def get_cnts_white_background(img, min_cnts_ratio=MIN_CNTS_RATIO):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray_inv = cv2.bitwise_not(img_gray)
+    _, bin = cv2.threshold(img_gray_inv, WHITE_BG_THRESH, 255, cv2.THRESH_BINARY)
+    cnts_list = get_cnts(bin, min_cnts_ratio)
     return cnts_list
