b3be58bd32098e05a8aba18dd295aa6217f7e9a6
matsuri@Rapport <matsurix28@gmail.com>
2024-06-28 17:48:00 +0900

    0628

--- lia/core/pickcell.py
+++ lia/core/pickcell.py
@@ -6,3 +6,9 @@ from lia.core.base import ImageCore
 class Pickcell(ImageCore):
     def __init__(self):
         self.num_cpu = os.cpu_count()
+
+    def set_param(self, **kwargs):
+        super().set_param(**kwargs)
+
+    def pick(self, input_std, input_var, std_cnt):
+        pass
